e41911857ac8c4f3459a9d4c39efcc25263b83c0
Paul David Tan <cryxuz@gmail.com>
2023-08-30 11:51:00 +1200

    solution for num 8

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -23,12 +23,17 @@ function Nav() {
         <IfAuthenticated>
           <NavButton onClick={handleSignOut}>Sign out</NavButton>
           {user && <p>Signed in as: {user?.nickname}</p>}
+          {/* Notice how user?.nickname has a question mark in front of it. This is called the optional chaining operator. 
+            When we sign in, the user object may still be null while the user object is being fetched from Auth0. This optional chaining operator will 
+            prevent the app from crashing if the user object is null (while the user is authenticated). */}
         </IfAuthenticated>
         <IfNotAuthenticated>
           <NavButton onClick={handleSignIn}>Sign in</NavButton>
         </IfNotAuthenticated>
       </NavGroup>
       <h1>Fruit FTW!</h1>
+      {console.log(user?.nickname)}
+      {console.log(user)}
     </>
   )
 }
