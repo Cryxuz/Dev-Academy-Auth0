940844b5578198986b47283c1bfbd9cdad1e6f17
Paul David Tan <cryxuz@gmail.com>
2023-08-30 11:30:57 +1200

    solution for num 6

--- client/components/Authenticated.tsx
+++ client/components/Authenticated.tsx
@@ -1,12 +1,17 @@
 // TODO: import useAuth0 function
+import { useAuth0 } from '@auth0/auth0-react'
 
-const useIsAuthenticated = () => {
-  // TODO: call the useAuth0 hook, destructure and return isAuthenticated
-  return true
-}
+//
 interface Props {
   children: React.ReactNode
 }
+
+const useIsAuthenticated = () => {
+  // TODO: call the useAuth0 hook, destructure and return isAuthenticated
+  const { isAuthenticated } = useAuth0()
+  return isAuthenticated
+}
+
 export function IfAuthenticated(props: Props) {
   const { children } = props
   return useIsAuthenticated() ? <>{children}</> : null
